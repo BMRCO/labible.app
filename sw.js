4a5f2a57da6f3e0ec3676ba7956446efc203d2c3
BMRCO <62664168+BMRCO@users.noreply.github.com>
2026-02-24 11:43:18 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,5 +2,4 @@ self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
-
 // Aucun cache, aucun fetch handler => pas de mode offline.

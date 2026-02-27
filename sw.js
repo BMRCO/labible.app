907e18ef63dd2a5b9e016b1baebd147d27e13d05
BMRCO <62664168+BMRCO@users.noreply.github.com>
2026-02-27 02:47:00 +0100

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-// Sem fetch => sem cache => sem offline
+// no fetch handler => no cache => no offline
